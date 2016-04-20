4ebc80c4cfb199858dbb250e6500768e1376a7b8
samuelbraley <sam.braley@squarecap.com>
2016-04-19 19:59:09 -0500

    added key rotation

---
+++ key_rotate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class key_rotate : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(new Vector3(transform.localPosition.x, 0, transform.localPosition.z), Vector3.up, Time.deltaTime * 100);
+    }
+}
