78b5cee50de1cb224c81f08732ced5a6497bf8b8
Corbin Rogerson <nicharesuk@gmail.com>
2016-04-27 18:57:41 -0500

    tile pairs

---
+++ TilePair.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TilePair {
+
+
+	tile tile_one;
+	tile tile_two;
+
+	//May need to make this more sorted, so that most upper left is always tile_one 
+	public TilePair(tile t1, tile t2)
+	{
+		tile_one = t1;
+		tile_two = t2;
+	}
+
+
+}
