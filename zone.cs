ff3521d4f21edd48bb8641a5246c265f84fdf675
Corbin Rogerson <nicharesuk@gmail.com>
2016-05-04 21:05:49 -0500

    corbin commit before pull

--- zone.cs
+++ zone.cs
@@ -4,18 +4,20 @@ using System.Collections.Generic;
 
 public class Zone {
 
-	static uint id = 0;
-	public Dictionary<Zone, List<TilePair>> neighbors; // public for now because it's simpler for testing
+	uint id;
+	public Dictionary<Zone, List<TilePair>> neighbors_tiles; // public for now because it's simpler for testing
+	public Zone[] neighbors;
 	List<tile> tiles; // List of tiles within this zone
 	List<Zone> connected; // Zones that are actually connected by a door
 
 
-	public Zone()
+	public Zone(uint id)
 	{
-		id++;
-		neighbors = new Dictionary<Zone, List<TilePair>>();
+		this.id = id;
+		neighbors_tiles = new Dictionary<Zone, List<TilePair>>();
 		tiles = new List<tile>();
 		connected = new List<Zone>();
+		neighbors = null;
 
 
 	}
